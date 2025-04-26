355824edaca82c85d392910c9e0e3ab1aad7abc5
murodjon <murodjonisroiljonov4@gmail.com>
2025-04-26 17:58:33 +0500

    ..

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -5,12 +5,14 @@ import React from "react";
 
 function Logo() {
   return (
-    <div className="flex gap-2 items-center">
+    <div>
       <Link href={"/"}>
-        <Image alt="logo" src={"/Logo.png"} width={32} height={32} />
-        <h2 className="font-redhat font-bold text-2xl leading-[150%] text-neutrals-100 space-x-[-1%]">
-          JobHuntly
-        </h2>
+        <div className="flex gap-2 items-center">
+          <Image alt="logo" src={"/Logo.png"} width={32} height={32} />
+          <h2 className="font-redhat font-bold text-2xl leading-[150%] text-neutrals-100 space-x-[-1%]">
+            JobHuntly
+          </h2>
+        </div>
       </Link>
     </div>
   );
