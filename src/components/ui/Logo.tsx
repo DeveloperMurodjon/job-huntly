27b825163dbea8b2af02345eb40aee886059f725
murodjon <murodjonisroiljonov4@gmail.com>
2025-04-26 17:49:49 +0500

    logo

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,14 +1,17 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Logo() {
   return (
     <div className="flex gap-2 items-center">
-      <Image alt="logo" src={"/Logo.png"} width={32} height={32} />
-      <h2 className="font-redhat font-bold text-2xl leading-[150%] text-neutrals-100 space-x-[-1%]">
-        JobHuntly
-      </h2>
+      <Link href={"/"}>
+        <Image alt="logo" src={"/Logo.png"} width={32} height={32} />
+        <h2 className="font-redhat font-bold text-2xl leading-[150%] text-neutrals-100 space-x-[-1%]">
+          JobHuntly
+        </h2>
+      </Link>
     </div>
   );
 }
